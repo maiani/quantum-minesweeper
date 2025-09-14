c1fe6b450f49b1ed35a392fd43081092e029edb2
Andrea Maiani <andreamaiani@gmail.com>
2025-09-14 22:27:14 +0200

    Add a deploy script

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,8 +1,10 @@
 # scripts/config.sh
+
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
 PROJECT_ID="qminesweeper"
 REGION="europe-west1"
 REPO="qms"
 SERVICE="quantum-minesweeper"
-
-DEMO_USER="demo"
-DEMO_PASS="nordita"
